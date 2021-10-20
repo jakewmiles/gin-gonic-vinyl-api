60687c87fa3c908eadf61d07d590d2cd261e7d4f
jakewmiles <jake.w.miles@gmail.com>
2021-10-20 22:39:54 +0100

    feat: create album struct and hard-coded data

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Geogaddi", Artist: "Boards of Canada", Price: 20.99},
+	{ID: "2", Title: "Ravedeath, 1972", Artist: "Tim Hecker", Price: 28.99},
+	{ID: "3", Title: "Kid A", Artist: "Radiohead", Price: 24.99},
+}
+
 func main() {
 
 }

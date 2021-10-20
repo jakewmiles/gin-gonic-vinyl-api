b4bf2640322d695968e864840b9e56b0bf9838f7
jakewmiles <jake.w.miles@gmail.com>
2021-10-20 22:40:25 +0100

    feat: add basic function returning all albums

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -11,6 +17,10 @@ var albums = []album{
 	{ID: "1", Title: "Geogaddi", Artist: "Boards of Canada", Price: 20.99},
 	{ID: "2", Title: "Ravedeath, 1972", Artist: "Tim Hecker", Price: 28.99},
 	{ID: "3", Title: "Kid A", Artist: "Radiohead", Price: 24.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
 
 func main() {

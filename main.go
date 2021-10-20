7160890a4a407b2e10903bdfa93f0f7247086114
jakewmiles <jake.w.miles@gmail.com>
2021-10-20 22:52:32 +0100

    feat: implement getalbumbyid with loop over data

--- main.go
+++ main.go
@@ -23,6 +23,18 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 	err := c.BindJSON(&newAlbum)
@@ -36,6 +48,7 @@ func postAlbums(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 	router.Run("localhost:8080")
 }
